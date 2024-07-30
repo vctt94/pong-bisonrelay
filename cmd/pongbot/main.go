67d7b4c1c61ddd34e9a2670a0b639ec255be4b35
vctt <victor.duarte.oliveira@gmail.com>
2024-07-30 16:14:43 -0300

    add render to plugin server

--- cmd/pongbot/main.go
+++ cmd/pongbot/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/companyzero/bisonrelay/clientplugin/grpctypes"
 	"github.com/companyzero/bisonrelay/clientrpc/jsonrpc"
@@ -23,7 +25,7 @@ import (
 )
 
 var (
-	flagURL = flag.String("url", "wss://127.0.0.1:7777/ws", "URL of the websocket endpoint")
+	flagURL = flag.String("url", "wss://127.0.0.1:7676/ws", "URL of the websocket endpoint")
 
 	flagServerCertPath = flag.String("servercert", "/home/pongbot/brclient/rpc.cert", "Path to rpc.cert file")
 	flagClientCertPath = flag.String("clientcert", "/home/pongbot/brclient/rpc-client.cert", "Path to rpc-client.cert file")
@@ -40,6 +42,38 @@ type PongPlugin struct {
 	UpdatesCh  map[string]chan *pong.GameUpdateBytes
 	PongClient map[string]pong.PongGame_InitClient
 	Stream     map[string]grpctypes.PluginService_InitServer
+}
+
+func (s *pluginServer) Render(ctx context.Context, data *grpctypes.RenderRequest) (*grpctypes.RenderResponse, error) {
+	gameState := pong.GameUpdate{}
+	if err := json.Unmarshal(data.Data, &gameState); err != nil {
+		return nil, fmt.Errorf("failed to decode game state: %v", err)
+	}
+
+	var gameView strings.Builder
+	for y := 0; y < int(gameState.GameHeight); y++ {
+		for x := 0; x < int(gameState.GameWidth); x++ {
+			ballX := int(gameState.BallX)
+			ballY := int(gameState.BallY)
+			switch {
+			case x == ballX && y == ballY:
+				gameView.WriteString("O")
+			case x == 0 && y >= int(gameState.P1Y) && y < int(gameState.P1Y)+int(gameState.P1Height):
+				gameView.WriteString("|")
+			case x == int(gameState.GameWidth)-1 && y >= int(gameState.P2Y) && y < int(gameState.P2Y)+int(gameState.P2Height):
+				gameView.WriteString("|")
+			default:
+				gameView.WriteString(" ")
+			}
+		}
+		gameView.WriteString("\n")
+	}
+	gameView.WriteString(fmt.Sprintf("Score: %d - %d\n", gameState.P1Score, gameState.P2Score))
+	gameView.WriteString("Controls: W/S and Arrow Keys - Q to quit")
+
+	return &grpctypes.RenderResponse{
+		Data: gameView.String(),
+	}, nil
 }
 
 type pluginServer struct {
